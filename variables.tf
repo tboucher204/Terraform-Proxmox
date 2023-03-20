a4f0a9da7fb26b4e6fc6631fb86c1db2b8a53ee8
tboucher204 <tboucher@live.com>
2023-03-19 21:53:12 -0400

    updated variables

--- variables.tf
+++ variables.tf
@@ -2,14 +2,14 @@ variable "pm_user" {
   description = "The username for the proxmox user"
   type        = string
   sensitive   = false
-  default     = "YOUR-PM-USER"
+  default     = "root"
 
 }
 variable "pm_password" {
   description = "The password for the proxmox user"
   type        = string
   sensitive   = true
-  default     = "YOUR-PM-PASSWORD"
+  default     = "P@ssw0rd1"
 }
 
 variable "pm_tls_insecure" {
@@ -21,13 +21,13 @@ variable "pm_tls_insecure" {
 variable "pm_host" {
   description = "The hostname or IP of the proxmox server"
   type        = string
-  default     = "YOUR-PM-HOST"
+  default     = "pve3.theboucher6.com"
 }
 
 variable "pm_node_name" {
   description = "name of the proxmox node to create the VMs on"
   type        = string
-  default     = "YOUR-PM-NODENAME"
+  default     = "pve3"
 }
 
 variable "pvt_key" {
@@ -38,73 +38,71 @@ variable "pvt_key" {
 
 variable "num_masters" {
   description = "Enter the number of Master VMs you want"
-  default = X
+  default = 1
 }
 
 variable "num_masters_mem" {
   description = "Enter the value for the amount of RAM for your masters. ie. 4096"
-  default = "XXXX"
+  default = "4096"
 }
 
 variable "master_disk_size" {
   description = "Enter the size of your Master node disks ie. 80G"
   type        = string
-  default     = "YOUR-MASTER-DISK-SIZE"
+  default     = "80G"
 }
 
 variable "master_disk_type" {
   description = "What interface type are you using? ie. scsi"
   type        = string
-  default     = "YOUR-MASTER-DISK-SIZE"
+  default     = "scsi"
 }
 
 variable "master_disk_location" {
   description = "Where do you want to store the disk on your host? ie. zfs-mirror, local, local-lvm, etc."
   type        = string
-  default     = "YOUR-MASTER-DISK-LOCATION"
+  default     = "Primary"
 }
 
 variable "num_nodes" {
   description = "Enter the number of VMs you want for worker nodes."
-  default = X
+  default = 2
 }
 
 variable "num_nodes_mem" {
   description = "Enter the value for the amount of RAM for your worker nodes. ie. 2048"
-  default = "xxxx"
+  default = "4096"
 }
 
 variable "node_disk_size" {
   description = "Enter the size of your Master node disks ie. 80G"
   type        = string
-  default     = "YOUR-NODE-DISK-SIZE"
+  default     = "80G"
 }
 
 variable "node_disk_type" {
   description = "What interface type are you using? ie. scsi"
   type        = string
-  default     = "YOUR-NODE-DISK-SIZE"
+  default     = "scsi"
 }
 
 variable "node_disk_location" {
   description = "Where do you want to store the disk on your host? ie. zfs-mirror, local, local-lvm, etc."
   type        = string
-  default     = "YOUR-NODE-DISK-LOCATION"
+  default     = "Primary"
 }
 
 variable "template_vm_name" {
   description = "Name of the template VM"
   type        = string
-  default     = "YOUR-PM-VM-TEMPLATE-NAME"
+  default     = "ubuntu-focal-2023-template"
 }
 
 variable "master_ips" {
   description = "List of ip addresses for master nodes"
   type        = list(string)
   default     = [
-    "a.a.a.a",
-    "b.b.b.b",
-    "c.c.c.c",
+    "192.168.1.80"
   ]
 }
 
@@ -112,20 +110,17 @@ variable "worker_ips" {
   description = "List of ip addresses for worker nodes"
   type        = list(string)
   default     = [  
-    "m.m.m.m",
-    "n.n.n.n",
-    "o.o.o.o",
-    "p.p.p.p",
-    "q.q.q.q",
+    "192.168.1.81",
+    "192.168.1.82"
   ]
 }
 
 variable "networkrange" {
   description = "Enter as 8,16,22,24,etc. hint: 10.0.0.0/8"
-  default = YOUR-NET-NUM
+  default = 24
 }
 
 variable "gateway" {
   description = "Enter your network gateway."
-  default = "YOUR-GATEWAY"
+  default = "192.168.1.1"
 }
